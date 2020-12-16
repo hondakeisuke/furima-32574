20da8298d8bf2185e913bf714567fa00dfe2f725
hondakeisuke <keisuke.h101@icloud.com>
2020-12-16 14:54:03 +0900

    ユーザー管理機能の実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname               {"test"}
+    email                  {"test@examlie"}
+    password               {"test000"}
+    password_confirmation  {password}
+    last_name              {"全"}
+    first_name             {"角"}
+    last_name_reading      {"ゼン"}
+    first_name_reading     {"カク"}
+    birthday               { Faker::Date.birthday(min_age: 5, max_age: 90) }
+  end
+end
