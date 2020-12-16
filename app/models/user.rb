38e27b2506b129221d92003e47508f30bee8cee8
hondakeisuke <keisuke.h101@icloud.com>
2020-12-16 15:57:17 +0900

    ユーザー管理機能修正（正規表現の追加等）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name	, presence: true
-  validates :last_name_reading	, presence: true
-  validates :first_name_reading	, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  validates :first_name	, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  validates :last_name_reading	, presence: true ,format: {with: /\A[ァ-ヴ]+\z/}
+  validates :first_name_reading	, presence: true, format: {with: /\A[ァ-ヴ]+\z/}
   validates :birthday	, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
